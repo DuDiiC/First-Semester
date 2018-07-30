5a22e708ae97ff9e137835a55fee25795dcc25dc
Maciej Dudek <maciejdudek96@wp.pl>
2018-07-30 10:02:36 +0200

    Delete unnecessary comments.

--- Programy/[i6] Niedopałki Piotra/Niedopałki Piotra.cpp
+++ Programy/[i6] Niedopałki Piotra/Niedopałki Piotra.cpp
@@ -17,33 +17,7 @@ int main() {
         }
         cout << spalonePapierosy << endl;
     }
-}
-JAKIŒ B£¥D KTOREGO NIE MOGE ZNALEZC
-int main() {
-    int n, k;
-    while(cin >> n >> k) {
-            int iloscSpalonychPapierosow = n;
-            int iloscDodatkowychNiedopalkow = 0;
-            while((int)n/k != 0) {
-                int iloscPapierosowZNiedopalkow = n / k;
-                if(n == k  && iloscDodatkowychNiedopalkow + 1 == k){
-                    n += iloscPapierosowZNiedopalkow;
-                }
-                iloscDodatkowychNiedopalkow += (n%k);
-                if(iloscDodatkowychNiedopalkow == k) {
-                    iloscPapierosowZNiedopalkow++;
-                    iloscDodatkowychNiedopalkow = 0;
-                } else if(iloscDodatkowychNiedopalkow > k) {
-                    iloscPapierosowZNiedopalkow++;
-                    iloscDodatkowychNiedopalkow -= k;
-                }
-                iloscSpalonychPapierosow += iloscPapierosowZNiedopalkow;
-                n = iloscPapierosowZNiedopalkow;
-            }
-        cout << iloscSpalonychPapierosow << endl;
-    }
-}
-*/
+}*/
 #include <iostream>
 using namespace std;
 int main() {
